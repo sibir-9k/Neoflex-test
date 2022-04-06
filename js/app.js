a2ce23884b6ff52a36f856624d9f8c9ea481cb72
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2022-04-06 23:44:35 +0300

    fix style basket + fix basket.html

--- js/app.js
+++ js/app.js
@@ -37,52 +37,92 @@ const headphones = [
   },
 ]
 
-var makeElement = function(tagName, className, text){
-  var element = document.createElement(tagName);
-  element.classList.add(className);
-  if (text) {
-    element.textContent = text;
-  }
-  return element;
-}
+// var makeElement = function(tagName, className, text){
+//   var element = document.createElement(tagName);
+//   element.classList.add(className);
+//   if (text) {
+//     element.textContent = text;
+//   }
+//   return element;
+// }
 
 
-var createCards = function (card) {
+// var createCards = function (card) {
 
  
-  var cardTop = makeElement('div', 'card__top');
-  listItem.appendChild(cardTop);
+//   var cardTop = makeElement('div', 'card__top');
+//   listItem.appendChild(cardTop);
 
-  var picture = makeElement('img')
-  picture.src = card.img;
-  picture.alt = card.title;
-  listItem.appendChild(picture)
+//   var picture = makeElement('img')
+//   picture.src = card.img;
+//   picture.alt = card.title;
+//   listItem.appendChild(picture)
 
-  var title = makeElement('h3', 'headphone__name', card.title);
-  listItem.appendChild(title)
+//   var title = makeElement('h3', 'headphone__name', card.title);
+//   listItem.appendChild(title)
 
-  var price = makeElement('span', 'headphone__price', card.price);
-  listItem.appendChild(price);
+//   var price = makeElement('span', 'headphone__price', card.price);
+//   listItem.appendChild(price);
 
-  var starRate = makeElement('img')
-  picture.src = '../img/star.svg';
-  listItem.appendChild(starRate)
+//   var starRate = makeElement('img')
+//   picture.src = '../img/star.svg';
+//   listItem.appendChild(starRate)
 
-  var rate = makeElement('div', 'headphone__rate', card.rate);
-  listItem.appendChild(rate);
+//   var rate = makeElement('div', 'headphone__rate', card.rate);
+//   listItem.appendChild(rate);
 
-  var buy = makeElement('button', 'headphone__buy')
+//   var buy = makeElement('button', 'headphone__buy')
   
 
-  return listItem;
-}
+//   return listItem;
+// }
 
-var renderCards = function (cards) {
-  var cardList = document.querySelector('.headphone__card');
+// var renderCards = function (cards) {
+//   var cardList = document.querySelector('.headphone__card');
 
-  var cardItem = createCards(cards[0])
+//   var cardItem = createCards(cards[0])
 
-  cardList.appendChild(cardItem);
-};
+//   cardList.appendChild(cardItem);
+// };
 
-renderCards(headphones);
+// renderCards(headphones);
+
+
+
+
+
+
+
+
+// function showMovies(data) {
+//   const moviesEl = document.querySelector(".headphone__card");
+
+
+//   data.films.forEach((card) => {
+//     const movieEl = document.createElement("div");
+//     movieEl.classList.add("card");
+//     movieEl.innerHTML = `
+//     <div class="headphone__card">
+//     <div class="card__top">
+//       <img src="${card.img}" alt="" />
+//     </div>
+//     <div class="card__bottom">
+//       <div class="headphone__title">
+//         <div class="headphone__name-price">
+//           <h3 class="headphone__name">${card.title}</h3>
+//           <span class="headphone__price">${card.price} ₽</span>
+//         </div>
+//         <div class="headphone-stars-buy">
+//           <span class="headphone__rate">
+//             <img src="./img/star.svg" alt="Оценка товара" />
+//             ${card.rate}
+//           </span>
+//           <button class="headphone__buy">Купить</button>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+//         `;
+//     moviesEl.appendChild(movieEl);
+//   });
+// }
