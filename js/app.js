ed6f5898ba6ec0dc3909c2d2b21194da0b747a37
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2022-04-07 16:21:54 +0300

    add new js code & fix html

--- js/app.js
+++ js/app.js
@@ -37,92 +37,63 @@ const headphones = [
   },
 ]
 
-// var makeElement = function(tagName, className, text){
-//   var element = document.createElement(tagName);
-//   element.classList.add(className);
-//   if (text) {
-//     element.textContent = text;
-//   }
-//   return element;
-// }
+// Функция которая будет принимать на вход три строки: имя тега (tagName), 
+// имя класса (className) и текстовое содержимое (text) элемента. 
+// Внутри неё будем создавать элемент с классом и текстом, а затем возвращать его наружу.
+var makeElement = function (tagName, className, text) {
+  var element = document.createElement(tagName);
+  element.classList.add(className);
+  if (text) {
+    element.textContent = text;
+  }
+  return element;
+}
 
+// Функция для создания карточки
+var createCard = function () {
+  var listItem = makeElement('li', 'card-block');
+  listItem.classList.add('card')
 
-// var createCards = function (card) {
-
+  var cardTop = makeElement('div', 'card__top')
+  listItem.appendChild(cardTop)
  
-//   var cardTop = makeElement('div', 'card__top');
-//   listItem.appendChild(cardTop);
+  var img = makeElement('img', 'card__img')
+  img.src = headphones[0].img;
+  img.alt = headphones[0].title;
+  listItem.appendChild(img)
 
-//   var picture = makeElement('img')
-//   picture.src = card.img;
-//   picture.alt = card.title;
-//   listItem.appendChild(picture)
+  var cardBottom = makeElement('div', 'card__bottom')
+  listItem.appendChild(cardBottom)
 
-//   var title = makeElement('h3', 'headphone__name', card.title);
-//   listItem.appendChild(title)
+  var headphoneTitle = makeElement('div', 'headphone__title')
+  listItem.appendChild(headphoneTitle);
 
-//   var price = makeElement('span', 'headphone__price', card.price);
-//   listItem.appendChild(price);
+  var headphoneNamePrice = makeElement('div', 'headphone__name-price')
+  listItem.appendChild(headphoneNamePrice);
 
-//   var starRate = makeElement('img')
-//   picture.src = '../img/star.svg';
-//   listItem.appendChild(starRate)
+  var title = makeElement('h3', 'headphone__name', headphones[0].title)
+  listItem.appendChild(title)
 
-//   var rate = makeElement('div', 'headphone__rate', card.rate);
-//   listItem.appendChild(rate);
+  var price = makeElement('span', 'headphone__price', headphones[0].price)
+  listItem.appendChild(price)
 
-//   var buy = makeElement('button', 'headphone__buy')
-  
+  var headphoneStarsBuy = makeElement('div', 'headphone-stars-buy')
+  listItem.appendChild(headphoneStarsBuy);
 
-//   return listItem;
-// }
+  var headphoneRate = makeElement('div', 'headphone__rate', headphones[0].rate)
+  listItem.appendChild(headphoneRate);
 
-// var renderCards = function (cards) {
-//   var cardList = document.querySelector('.headphone__card');
+  var starImg = makeElement('img')
+  starImg.src = './img/star.svg'
+  starImg.alt = 'Рейтинг'
+  listItem.appendChild(starImg)
 
-//   var cardItem = createCards(cards[0])
+  var headphoneBuy = makeElement('button', 'headphone__buy', 'Купить')
+  listItem.appendChild(headphoneBuy);
 
-//   cardList.appendChild(cardItem);
-// };
+  return listItem;
+}
 
-// renderCards(headphones);
-
-
-
-
-
-
-
-
-// function showMovies(data) {
-//   const moviesEl = document.querySelector(".headphone__card");
-
-
-//   data.films.forEach((card) => {
-//     const movieEl = document.createElement("div");
-//     movieEl.classList.add("card");
-//     movieEl.innerHTML = `
-//     <div class="headphone__card">
-//     <div class="card__top">
-//       <img src="${card.img}" alt="" />
-//     </div>
-//     <div class="card__bottom">
-//       <div class="headphone__title">
-//         <div class="headphone__name-price">
-//           <h3 class="headphone__name">${card.title}</h3>
-//           <span class="headphone__price">${card.price} ₽</span>
-//         </div>
-//         <div class="headphone-stars-buy">
-//           <span class="headphone__rate">
-//             <img src="./img/star.svg" alt="Оценка товара" />
-//             ${card.rate}
-//           </span>
-//           <button class="headphone__buy">Купить</button>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-//         `;
-//     moviesEl.appendChild(movieEl);
-//   });
-// }
+var cardList = document.querySelector('.cards')
+var cardItem = createCard();
+cardList.appendChild(cardItem)
